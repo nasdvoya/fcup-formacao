930fe5514fc4495088cb304cea8e940886c9df3b
sasha <nasdvoya@proton.me>
2024-11-05 20:54:22 +0000

    Simple random number game

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
-use rand::random;
+use rand::{random, Rng};
 
 fn main() {
     println!("Computer says what?");
-    let computers_says: i16 = random();
+    let computer_says: u8 = rand::thread_rng().gen_range(1..=3);
+
+    let mut input = String::new();
+    let _answer = std::io::stdin().read_line(&mut input).expect("Failed to read line");
+
+    if input.trim() == computer_says.to_string() {
+        println!("You guessed right! {} is the correct answer", computer_says)
+    } else {
+        println!("Computers says no.. {} is the correct answer", computer_says)
+    }
 }
