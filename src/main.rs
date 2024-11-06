682d30ef605844a0f781fb4aae4c6da1f92aa05e
sasha <nasdvoya@proton.me>
2024-11-06 23:56:03 +0000

    guessing game

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,17 @@
 use rand::{random, Rng};
 
 fn main() {
+    //one_try_guess_game();
+    endless_guess_game();
+}
+
+fn tupulo() {
+    let tuple = (500, 100);
+    let (x, y) = tuple;
+    println!("Isto vai destrui o tupulo, Valor {x} , {y}");
+}
+
+fn one_try_guess_game() -> () {
     println!("Computer says what?");
     let computer_says: u8 = rand::thread_rng().gen_range(1..=3);
 
@@ -14,8 +25,21 @@ fn main() {
     }
 }
 
-fn tupulo() {
-    let tuple = (500, 100);
-    let (x, y) = tuple;
-    println!("Isto vai destrui o tupulo, Valor {x} , {y}");
+fn endless_guess_game() -> () {
+    let number_to_guess: u8 = rand::thread_rng().gen_range(1..=2);
+    let mut input = String::new();
+
+    println!("I'm thinking of a number between 1 and 6");
+
+    while input.trim() != number_to_guess.to_string() {
+        println!("input is {}", input);
+        std::io::stdin().read_line(&mut input).expect("Failed to read line");
+        println!("Input is {}.ddd", input);
+
+        if input.trim() == number_to_guess.to_string() {
+            println!("You guessed it!")
+        } else {
+            println!("Wrong! Try again.")
+        }
+    }
 }
